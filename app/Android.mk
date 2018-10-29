51da04ac474cf67382f54c789bba1b0dbbfd9cf2
pakzan <pakzzzan@hotmail.com>
2018-10-29 16:03:55 +0800

    Added debug-apk

--- app/Android.mk
+++ app/Android.mk
@@ -4,6 +4,13 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := app
 LOCAL_LDFLAGS := -Wl,--build-id
 LOCAL_SRC_FILES := \
+	C:\Users\tpz\AndroidStudioProjects\CardRecog\app\src\main\jniLibs\arm64-v8a\libopencv_java3.so\
+	C:\Users\tpz\AndroidStudioProjects\CardRecog\app\src\main\jniLibs\armeabi\libopencv_java3.so\
+	C:\Users\tpz\AndroidStudioProjects\CardRecog\app\src\main\jniLibs\armeabi-v7a\libopencv_java3.so\
+	C:\Users\tpz\AndroidStudioProjects\CardRecog\app\src\main\jniLibs\mips\libopencv_java3.so\
+	C:\Users\tpz\AndroidStudioProjects\CardRecog\app\src\main\jniLibs\mips64\libopencv_java3.so\
+	C:\Users\tpz\AndroidStudioProjects\CardRecog\app\src\main\jniLibs\x86\libopencv_java3.so\
+	C:\Users\tpz\AndroidStudioProjects\CardRecog\app\src\main\jniLibs\x86_64\libopencv_java3.so\
 
 LOCAL_C_INCLUDES += C:\Users\tpz\AndroidStudioProjects\CardRecog\app\src\main\jni
 LOCAL_C_INCLUDES += C:\Users\tpz\AndroidStudioProjects\CardRecog\app\src\main\jniLibs
